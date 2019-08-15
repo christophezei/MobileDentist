12544969c3a8298998cbd22d47f4cc54756ad061
liangyuandg <liangyuandg@g.ucla.edu>
2019-08-14 20:02:47 -0700

    fix typos in classifcations

--- engines/engine_simple_infer_Classification.py
+++ engines/engine_simple_infer_Classification.py
@@ -1,7 +1,10 @@
 import os
+import os.path as osp
+import mmcv
 import torch
 
-from classifiers.GradCam import GradCAM, to_heatmap
+from classifiers.GradCam import GradCAM
+from classifiers.GradCam import save_gradcam, save_gradcam_over_image
 from classifiers.VGG16_Classifier import VGGClassifier
 from utils.checkpoint import load_checkpoint
 from cores.pre_processing.image_transform_de import ImageTransform, image_transfer_back
@@ -40,7 +43,7 @@ img_transform_cfg = \
 output_path = dir_path+'/visualization'
 
 
-def inference_func_CPU(raw_img):
+def main():
     # define the model and restore checkpoint
     model = VGGClassifier(
         with_bn=False,
@@ -73,6 +76,50 @@ def inference_func_CPU(raw_img):
     model.to(device)
     model.eval()
 
+    # backbone
+    # backbone.features
+    # backbone.features.0
+    # backbone.features.1
+    # backbone.features.2
+    # backbone.features.3
+    # backbone.features.4
+    # backbone.features.5
+    # backbone.features.6
+    # backbone.features.7
+    # backbone.features.8
+    # backbone.features.9
+    # backbone.features.10
+    # backbone.features.11
+    # backbone.features.12
+    # backbone.features.13
+    # backbone.features.14
+    # backbone.features.15
+    # backbone.features.16
+    # backbone.features.17
+    # backbone.features.18
+    # backbone.features.19
+    # backbone.features.20
+    # backbone.features.21
+    # backbone.features.22
+    # backbone.features.23
+    # backbone.features.24
+    # backbone.features.25
+    # backbone.features.26
+    # backbone.features.27
+    # backbone.features.28
+    # backbone.features.29
+    # backbone.features.30
+    # classifier
+    # classifier.0
+    # classifier.1
+    # classifier.2
+    # classifier.3
+    # classifier.4
+    # classifier.5
+    # classifier.6
+    # for name, module in model.named_modules():
+    #     print(name)
+
     img_transform = ImageTransform(
         mean=img_transform_cfg['mean'],
         std=img_transform_cfg['std'],
@@ -80,6 +127,11 @@ def inference_func_CPU(raw_img):
     )
 
     # inference
+    # read image
+    raw_img = mmcv.imread(img_path)
+    ori_shape = raw_img.shape
+    print(ori_shape)
+
     # transform image
     img, img_shape, pad_shape, scale_factor = img_transform(
         img=raw_img,
@@ -88,47 +140,80 @@ def inference_func_CPU(raw_img):
         pad_val=img_transform_cfg['pad_values'],
         keep_ratio=img_transform_cfg['resize_keep_ratio'],
     )
-    img = to_tensor(img).to(device).unsqueeze(0)
 
-    target_layers = ["backbone.features.30"]
+    img = to_tensor(img).to(device).unsqueeze(0)
+    img_meta = [
+        dict(
+            ori_shape=ori_shape,
+            img_shape=img_shape,
+            pad_shape=pad_shape,
+            scale_factor=scale_factor,
+            flip=img_transform_cfg['flip']
+        )
+    ]
+
+    # with torch.no_grad():
+    #     result = model.forward_test(img=img, img_meta=img_meta, rescale=False)
+
+    target_layers = ["backbone.features.30", "backbone.features.29", "backbone.features.28", "backbone.features.27"]
+    target_class = 0
     gcam = GradCAM(model=model)
     probs = gcam.forward(img)
-    final_probs = probs.cpu().numpy()
-
-    # heatmap for class 0
-    target_class = 0
     ids_ = torch.LongTensor([[target_class]]).to(device)
     gcam.backward(ids=ids_)
-
-    # Grad-CAM
-    # regions: [H, W]
-    # raw_img: [H, W, 3]
-    # ori_regions_heatmap: [H, W, 3]
-    for target_layer in target_layers:
-        regions_0 = gcam.generate(target_layer=target_layer)
-        regions_0 = regions_0[0, 0].cpu().numpy()
-        ori_regions_0 = image_transfer_back(
-            img=regions_0,
-            scale=scale_factor,
-            cur_shape=regions_0.shape,
-            ori_shape=raw_img.shape[0:2]
-        )
-        ori_regions_heatmap_0 = to_heatmap(ori_regions_0)
-
-    # heatmap for class 1
-    target_class = 1
-    ids_ = torch.LongTensor([[target_class]]).to(device)
-    gcam.backward(ids=ids_)
+    print(probs)
 
     for target_layer in target_layers:
-        regions_1 = gcam.generate(target_layer=target_layer)
-        regions_1 = regions_1[0, 0].cpu().numpy()
-        ori_regions_1 = image_transfer_back(
-            img=regions_1,
+        print("Generating Grad-CAM @{}".format(target_layer))
+
+        # Grad-CAM
+        # regions: [1, 1, H, W]
+        # raw_img: [H, W, 3]
+        regions = gcam.generate(target_layer=target_layer)
+        regions = regions[0, 0].cpu().numpy()
+
+        ori_regions = image_transfer_back(
+            img=regions,
             scale=scale_factor,
-            cur_shape=regions_1.shape,
+            cur_shape=regions.shape,
             ori_shape=raw_img.shape[0:2]
         )
-        ori_regions_heatmap_1 = to_heatmap(ori_regions_1)
 
-    return final_probs, ori_regions_0, ori_regions_1, ori_regions_heatmap_0, ori_regions_heatmap_1
+        save_gradcam(
+            filename=osp.join(
+                output_path,
+                "gradcam-{}.png".format(
+                    target_layer
+                ),
+            ),
+            gcam=regions,
+            paper_cmap=True,
+        )
+        save_gradcam(
+            filename=osp.join(
+                output_path,
+                "orishape-gradcam-{}.png".format(
+                    target_layer
+                ),
+            ),
+            gcam=ori_regions,
+            paper_cmap=True,
+        )
+        save_gradcam_over_image(
+            filename=osp.join(
+                output_path,
+                "gradcam_over_img-{}.png".format(
+                    target_layer
+                ),
+            ),
+            gcam=ori_regions,
+            raw_image=raw_img,
+            paper_cmap=False,
+        )
+
+
+if __name__ == '__main__':
+    main()
+
+
+
--- server_deploy/inference_Classification_func_CPU.py
+++ server_deploy/inference_Classification_func_CPU.py
@@ -1,10 +1,7 @@
 import os
-import os.path as osp
-import mmcv
 import torch
 
-from classifiers.GradCam import GradCAM
-from classifiers.GradCam import save_gradcam, save_gradcam_over_image
+from classifiers.GradCam import GradCAM, to_heatmap
 from classifiers.VGG16_Classifier import VGGClassifier
 from utils.checkpoint import load_checkpoint
 from cores.pre_processing.image_transform_de import ImageTransform, image_transfer_back
@@ -43,7 +40,7 @@ img_transform_cfg = \
 output_path = dir_path+'/visualization'
 
 
-def main():
+def inference_func_CPU(raw_img):
     # define the model and restore checkpoint
     model = VGGClassifier(
         with_bn=False,
@@ -76,50 +73,6 @@ def main():
     model.to(device)
     model.eval()
 
-    # backbone
-    # backbone.features
-    # backbone.features.0
-    # backbone.features.1
-    # backbone.features.2
-    # backbone.features.3
-    # backbone.features.4
-    # backbone.features.5
-    # backbone.features.6
-    # backbone.features.7
-    # backbone.features.8
-    # backbone.features.9
-    # backbone.features.10
-    # backbone.features.11
-    # backbone.features.12
-    # backbone.features.13
-    # backbone.features.14
-    # backbone.features.15
-    # backbone.features.16
-    # backbone.features.17
-    # backbone.features.18
-    # backbone.features.19
-    # backbone.features.20
-    # backbone.features.21
-    # backbone.features.22
-    # backbone.features.23
-    # backbone.features.24
-    # backbone.features.25
-    # backbone.features.26
-    # backbone.features.27
-    # backbone.features.28
-    # backbone.features.29
-    # backbone.features.30
-    # classifier
-    # classifier.0
-    # classifier.1
-    # classifier.2
-    # classifier.3
-    # classifier.4
-    # classifier.5
-    # classifier.6
-    # for name, module in model.named_modules():
-    #     print(name)
-
     img_transform = ImageTransform(
         mean=img_transform_cfg['mean'],
         std=img_transform_cfg['std'],
@@ -127,11 +80,6 @@ def main():
     )
 
     # inference
-    # read image
-    raw_img = mmcv.imread(img_path)
-    ori_shape = raw_img.shape
-    print(ori_shape)
-
     # transform image
     img, img_shape, pad_shape, scale_factor = img_transform(
         img=raw_img,
@@ -140,77 +88,47 @@ def main():
         pad_val=img_transform_cfg['pad_values'],
         keep_ratio=img_transform_cfg['resize_keep_ratio'],
     )
-
     img = to_tensor(img).to(device).unsqueeze(0)
-    img_meta = [
-        dict(
-            ori_shape=ori_shape,
-            img_shape=img_shape,
-            pad_shape=pad_shape,
-            scale_factor=scale_factor,
-            flip=img_transform_cfg['flip']
-        )
-    ]
 
-    # with torch.no_grad():
-    #     result = model.forward_test(img=img, img_meta=img_meta, rescale=False)
-
-    target_layers = ["backbone.features.30", "backbone.features.29", "backbone.features.28", "backbone.features.27"]
-    target_class = 0
+    target_layers = ["backbone.features.30"]
     gcam = GradCAM(model=model)
     probs = gcam.forward(img)
+    final_probs = probs.cpu().numpy()
+
+    # heatmap for class 0
+    target_class = 0
     ids_ = torch.LongTensor([[target_class]]).to(device)
     gcam.backward(ids=ids_)
-    print(probs)
 
+    # Grad-CAM
+    # regions: [H, W]
+    # raw_img: [H, W, 3]
+    # ori_regions_heatmap: [H, W, 3]
     for target_layer in target_layers:
-        print("Generating Grad-CAM @{}".format(target_layer))
-
-        # Grad-CAM
-        # regions: [1, 1, H, W]
-        # raw_img: [H, W, 3]
-        regions = gcam.generate(target_layer=target_layer)
-        regions = regions[0, 0].cpu().numpy()
-
-        ori_regions = image_transfer_back(
-            img=regions,
+        regions_0 = gcam.generate(target_layer=target_layer)
+        regions_0 = regions_0[0, 0].cpu().numpy()
+        ori_regions_0 = image_transfer_back(
+            img=regions_0,
             scale=scale_factor,
-            cur_shape=regions.shape,
+            cur_shape=regions_0.shape,
             ori_shape=raw_img.shape[0:2]
         )
+        ori_regions_heatmap_0 = to_heatmap(ori_regions_0)
 
-        save_gradcam(
-            filename=osp.join(
-                output_path,
-                "gradcam-{}.png".format(
-                    target_layer
-                ),
-            ),
-            gcam=regions,
-            paper_cmap=True,
-        )
-        save_gradcam(
-            filename=osp.join(
-                output_path,
-                "orishape-gradcam-{}.png".format(
-                    target_layer
-                ),
-            ),
-            gcam=ori_regions,
-            paper_cmap=True,
-        )
-        save_gradcam_over_image(
-            filename=osp.join(
-                output_path,
-                "gradcam_over_img-{}.png".format(
-                    target_layer
-                ),
-            ),
-            gcam=ori_regions,
-            raw_image=raw_img,
-            paper_cmap=False,
-        )
+    # heatmap for class 1
+    target_class = 1
+    ids_ = torch.LongTensor([[target_class]]).to(device)
+    gcam.backward(ids=ids_)
 
+    for target_layer in target_layers:
+        regions_1 = gcam.generate(target_layer=target_layer)
+        regions_1 = regions_1[0, 0].cpu().numpy()
+        ori_regions_1 = image_transfer_back(
+            img=regions_1,
+            scale=scale_factor,
+            cur_shape=regions_1.shape,
+            ori_shape=raw_img.shape[0:2]
+        )
+        ori_regions_heatmap_1 = to_heatmap(ori_regions_1)
 
-if __name__ == '__main__':
-    main()
+    return final_probs, ori_regions_0, ori_regions_1, ori_regions_heatmap_0, ori_regions_heatmap_1
